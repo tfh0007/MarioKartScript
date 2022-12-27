d4f16c7b6bbe16af97ec5736f4250a5f67c622e1
Thomas Hansknecht <tfh0007@auburn.edu>
2022-12-27 02:23:23 -0600

    Add files via upload

--- MarioKartAutomationArd1.8.18.ino
+++ MarioKartAutomationArd1.8.18.ino
@@ -8,13 +8,14 @@ const int joystickY  = A1;
 const int joyStickButtonPress = 1023;
 const int joyStickLowThreashold = 300;
 const int joyStickHighThreashold = 650;
-const int validScriptNums[5] {1, 2, 3, 4, 5};
+const int validScriptNums[6] {1, 2, 3, 4, 5, 6};
 int lastButtonPress = -1;
 int curScriptNum = 1;
 int confirmScriptNum = false;
 bool waitForInput = true;
 bool automatedKartSelectionAlreadyUsed = false;
 bool characterAlreadySelected = false;
+bool currentlyPlayer1 = false;
 
 // Display Clock setup
 const int clock = 2;
@@ -179,6 +180,13 @@ void waitForScriptSelection()
 
 */
 
+void DisplayForAll(int value) {
+  tm.display(0,value);
+  tm.display(1,value);
+  tm.display(2,value);
+  tm.display(3,value);
+}
+
 bool checkIfCurSelectionValid()
 {
   // Here we need to check for situations such as blue shells only which is invalid
@@ -187,123 +195,6 @@ bool checkIfCurSelectionValid()
 
 }
 
-void ScriptForPickingRandomItems()
-{
-  // Clear out the display
-  tm.display(0, 16);
-  tm.display(1, 16);
-  tm.display(2, 16);
-  tm.display(3, 16);
-  delay(1000);
-  //tm.clearDisplay();
-  //delay(1000);
-
-
-  Serial.begin(9600);
-  // The item selection grid layout
-  /*
-    {
-    Banna      ,    Triple Banna ,    Green Shell ,     Triple Green Shell,       Red Shell,       Triple Red Shell
-    Blue Shell ,             Bomb,        mushroom,        Triple Mushroom,       Golden mushroom, Bullet Bill
-    Squid      ,       lightning ,            star,            fire flower,       bomarang,        pirana plant
-    boom box   ,    crazy 8,                  coin,                    boo
-
-    }
-
-  */
-  int board[4][6] = {
-    {0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0}
-  };
-
-  int numberOfPicks = random(1, 25); // Randomly choose the number of items to select // The top number is not included while the bottom one is
-  int itemsSelected = 0;
-  Serial.print(" numberOfPicks = ");
-  Serial.println(numberOfPicks);
-  Serial.println("-----------------");
-
-  delay(2000);
-  DisplayFullTwoDigitNumber(numberOfPicks);
-  delay(1000);
-
-
-
-
-
-  bool validSelection = false;
-  while (itemsSelected < numberOfPicks)
-  {
-    validSelection = false;
-    // We need to keep looping until we have a valid item selection even if we have to go over the number
-    // of picks we wanted
-    while (validSelection == false) {
-      int randomRow = random(0, 4);
-      int randomCol = random(0, 6);
-      Serial.print(" Trying to select: ["); Serial.print(randomRow); Serial.print(","); Serial.print(randomCol); Serial.print("]");
-      tm.clearDisplay();
-      delay(500);
-      // We are now selecting this square
-      if (board[randomRow][randomCol] == 0)
-      {
-        board[randomRow][randomCol] = 1;
-        Serial.println("--> Accepted");
-        // Display that we picked this row col pair
-
-        tm.display(0, 10); // a -> accepted
-        tm.display(2, randomRow);
-        tm.display(3, randomCol);
-        validSelection = checkIfCurSelectionValid();
-        itemsSelected++;
-      }
-      else {
-        Serial.println("--> Rejected");
-        tm.display(0, 14); // e -> exculded
-        tm.display(2, randomRow);
-        tm.display(3, randomCol);
-
-      }
-
-      delay(500);
-      tm.clearDisplay();
-      // We need to check if the number is bigger than 10 or 20
-      DisplayFullTwoDigitNumber(itemsSelected);
-      delay(500);
-
-
-
-    }
-
-  }
-  // Now let's display the resulting array
-
-  Serial.println("{");
-
-  // loop through array's rows
-  for ( int i = 0; i < 4; i++ ) {
-    // loop through columns of current row
-    for ( int j = 0; j < 6; j++ ) {
-      Serial.print (board[ i ][ j ] );
-      Serial.print(" ") ; // start new line of output
-    }
-    Serial.println();
-  }
-
-  Serial.println("}");
-  tm.clearDisplay();
-
-  tm.display(0, 10);
-  tm.display(1, 10);
-  tm.display(2, 10);
-  tm.display(3, 10);
-
-
-  tone(buzzer, 2000, 2000); // activate the buzzer
-  delay(5000);
-
-
-}
 void DisplayFullTwoDigitNumber(int item)
 {
   if (item < 10) {
@@ -372,9 +263,18 @@ void DPadTest()
   tm.clearDisplay();
   //Serial.println("Press a Button:");
   lastButtonPress = -1;
-  int curButton = -1;
+  
   // Set the arduino to wait for input
+
+  tone(buzzer, 2000, 2000); // activate the buzzer
+  WaitForInput();
+}
+
+void WaitForInput() {
+  int curButton = -1;
+  
   while (true) {
+    DisplayForAll(12);
     int joyStickXValue = analogRead(joystickX);
     int joyStickYValue = analogRead(joystickY);
     // Button inner press
@@ -450,21 +350,16 @@ void DPadTest()
       }
 
     }
+    ClearDisplay();
   }
-
-
-}
-
-
-
-
+  }
 
 void runMainProgram()
 {
   // Script for picking random items from the custom items menu
   if (curScriptNum == 1)
   {
-    ScriptForPickingRandomItems();
+    Automated1PlayerPairing();
 
   }
   else if (curScriptNum == 2)
@@ -482,8 +377,54 @@ void runMainProgram()
   else if (curScriptNum == 5) {
     AutomatedMenuSelection();
   }
+  else if (curScriptNum == 6) {
+    AutomatedItemSelection();
+  }
 
 
+}
+
+void Automated1PlayerPairing() {
+  // We need to activate the switch menu
+    pushButton(Button::HOME,1000);
+
+    // Now we need to move down twice
+    pushHat(Hat::DOWN, 200);
+    pushHat(Hat::DOWN, 200);
+
+    // Now we need to move right 6 times and then left twice to get to the controller settings
+    for(int i = 0; i < 6; i++) { 
+      pushHat(Hat::RIGHT, 50);
+    }
+
+    for(int i = 0; i < 2; i++) {
+      pushHat(Hat::LEFT, 50);
+    }
+    // Now we click A for the controller options
+    pushButton(Button::A,500);
+    // The first option will be to Change Grip/Order
+    pushButton(Button::A,500);
+
+    
+    // Now we are at the pairing screen
+
+    
+      // We need to pair the controller then exit back to the game
+      pushButton(Button::A,1000);
+      // Now that the controller is paired as the first player we need to go back to the switch menu
+      pushButton(Button::A,1000);
+      //Now we can go back to th main menu of the switch
+      pushButton(Button::B,1000);
+      //Finally we can return to the game by pressing the A button again
+      pushButton(Button::B,1000);
+      
+      pushButton(Button::HOME,1000);
+      currentlyPlayer1 = true;
+      characterAlreadySelected = false;
+      automatedKartSelectionAlreadyUsed = false;
+    
+    
+    
 }
 
 void AutomatedCharacterSelection()
@@ -582,6 +523,7 @@ void AutomatedKartSelection()
   pushButton(Button::A, 50);
 
  automatedKartSelectionAlreadyUsed = true;
+ characterAlreadySelected = false;
 }
 
 void AutomatedMenuSelection() {
@@ -643,7 +585,194 @@ void AutomatedMenuSelection() {
    pushButton(Button::A, 50); // Confirm the settings
    delay(1000);
    pushButton(Button::A, 50); // We may be in teams mode so we need to click again
+   characterAlreadySelected = false;
+   automatedKartSelectionAlreadyUsed = false;
   
+}
+
+void AutomatedItemSelection()
+{
+  // Clear out the display
+  ClearDisplay();
+  
+
+  // This script will only work properly if the the cursor starts at 0,0 and all items are disabled
+
+  // The item selection grid layout
+  /*
+    {
+    Banna      ,    Triple Banna ,    Green Shell ,     Triple Green Shell,       Red Shell,       Triple Red Shell
+    Blue Shell ,             Bomb,        mushroom,        Triple Mushroom,       Golden mushroom, Bullet Bill
+    Squid      ,       lightning ,            star,            fire flower,       bomarang,        pirana plant
+    boom box   ,    crazy 8,                  coin,                    boo
+
+    }
+
+  */
+  int board[4][6] = {
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+  };
+
+  int numberOfPicks = random(1, 25); // Randomly choose the number of items to select // The top number is not included while the bottom one is
+  int itemsSelected = 0;
+  Serial.print(" numberOfPicks = ");
+  Serial.println(numberOfPicks);
+  Serial.println("-----------------");
+
+  delay(2000);
+  DisplayFullTwoDigitNumber(numberOfPicks);
+  delay(1000);
+
+
+
+
+
+  int curRow = 0;
+  int curCol = 0;
+  while (itemsSelected < numberOfPicks)
+  {
+    // We need to keep looping until we have a valid item selection even if we have to go over the number
+    // of picks we wanted
+    
+      int randomRow = random(0, 4);
+      int randomCol = random(0, 6);
+      Serial.print(" Trying to select: ["); Serial.print(randomRow); Serial.print(","); Serial.print(randomCol); Serial.print("]");
+      tm.clearDisplay();
+      delay(500);
+      // We are now selecting this square
+      if (board[randomRow][randomCol] == 0)
+      {
+        tm.display(0, 10); // a -> accepted
+        tm.display(2, randomRow);
+        tm.display(3, randomCol);
+        // Now we need to navigate to the correct square
+        while(curRow != randomRow || curCol != randomCol) {
+          /* Possible moves 
+           Hat::UP_LEFT
+           Hat::UP_RIGHT
+           Hat::DOWN_RIGHT
+           Hat::DOWN_LEFT
+           
+           Hat::UP
+           Hat::RIGHT
+           Hat::DOWN
+           Hat::LEFT
+          */
+           
+           // UP_LEFT
+          if(curRow > randomRow && curCol > randomCol) {
+            pushHat(Hat::UP_LEFT, 50);
+            curRow--;
+            curCol--;
+          }
+          // UP_RIGHT
+          else if (curRow > randomRow && curCol < randomCol) {
+            pushHat(Hat::UP_RIGHT, 50);
+            curRow--;
+            curCol++;
+          }
+          // DOWN_RIGHT
+          else if (curRow < randomRow && curCol < randomCol) {
+            pushHat(Hat::DOWN_RIGHT, 50);
+            curRow++;
+            curCol++;
+          }
+          // DOWN_LEFT
+          else if (curRow < randomRow && curCol > randomCol) {
+            pushHat(Hat::DOWN_LEFT, 50);
+            curRow ++;
+            curCol --;
+          }
+          
+          // RIGHT
+          if (curCol < randomCol) {
+            pushHat(Hat::RIGHT, 50);
+            curCol = curCol+1;
+          }
+          // LEFT
+          else if (curCol > randomCol) {
+            pushHat(Hat::LEFT, 50);
+            curCol = curCol- 1;
+          }
+          // DOWN
+          if (curRow < randomRow) {
+            pushHat(Hat::DOWN, 50);
+            curRow = curRow + 1;
+          }
+          // UP
+          else if (curRow > randomRow) {
+            pushHat(Hat::UP, 50);
+            curRow = curRow - 1;
+          }
+        }
+        pushButton(Button::A, 500); // Select the item
+
+        
+        board[randomRow][randomCol] = 1;
+        Serial.println("--> Accepted");
+        // Display that we picked this row col pair
+
+        
+        itemsSelected++;
+      }
+      else {
+        Serial.println("--> Rejected");
+        tm.display(0, 14); // e -> exculded
+        tm.display(2, randomRow);
+        tm.display(3, randomCol);
+        delay(500);
+
+      }
+      tm.clearDisplay();
+      // We need to check if the number is bigger than 10 or 20
+      DisplayFullTwoDigitNumber(itemsSelected);
+      delay(500);
+
+
+
+    
+
+  }
+  // Now let's display the resulting array
+
+  Serial.println("{");
+
+  // loop through array's rows
+  for ( int i = 0; i < 4; i++ ) {
+    // loop through columns of current row
+    for ( int j = 0; j < 6; j++ ) {
+      Serial.print (board[ i ][ j ] );
+      Serial.print(" ") ; // start new line of output
+    }
+    Serial.println();
+  }
+
+  Serial.println("}");
+  ClearDisplay();
+
+  tm.display(0, 10);
+  tm.display(1, 10);
+  tm.display(2, 10);
+  tm.display(3, 10);
+
+
+  tone(buzzer, 2000, 2000); // activate the buzzer
+  delay(3000);
+
+  // Once the items arre selected we need to scroll down to the bottom and click OK
+  while(curRow != 5) {
+    pushHat(Hat::DOWN, 200);
+    curRow ++;
+  }
+
+  pushButton(Button::A, 500);
+  characterAlreadySelected = false;
+  automatedKartSelectionAlreadyUsed = false;
+
+
 }
 
 void buttonMashing()
